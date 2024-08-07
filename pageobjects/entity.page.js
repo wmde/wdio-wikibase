b060ff8653ab4e16ad0ea9bc5f60240f34730609
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2024-08-07 17:24:49 +0200

    EntityPage: Wait for MediaWiki to be ready
    We've observed test failures indicating that MediaWiki's JS hadn't fully
    loaded yet after using EntityPage.open(). Waiting for the mediawiki.base
    ResourceLoader module should prevent that.
    
    Bug: T370139

--- pageobjects/entity.page.js
+++ pageobjects/entity.page.js
@@ -1,10 +1,12 @@
 const Page = require( 'wdio-mediawiki/Page' );
+const Util = require( 'wdio-mediawiki/Util' );
 
 class EntityPage extends Page {
 
 	async open( entityId ) {
 		await super.openTitle( `Special:EntityPage/${entityId}` );
 
+		await Util.waitForModuleState( 'mediawiki.base' ); // wait for mediawiki to be ready
 		await browser.executeAsync( ( done ) => {
 			mw.loader.using( 'mediawiki.cookie' ).then( () => { // eslint-disable-line no-undef
 				mw.cookie.set( 'wikibase-no-anonymouseditwarning', 'true' ); // eslint-disable-line no-undef
